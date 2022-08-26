be60d88559910c8dba415573e995295eb8532146
Grey601 <grev.gr@hotmail.com>
2022-08-27 01:17:35 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.surename = "Сергеевич";
+        post.patronymic = "Петров";
+        post.phone = "+7 (916) 123-45-67";
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1983;
+        post.passport = "4506 №456765";
+        // заполните другие поля
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String surename;
+    public String patronymic;
+    public String phone;
+    public FormDate birthday;
+    public String passport;
+    // добавьте другие поля
+}
